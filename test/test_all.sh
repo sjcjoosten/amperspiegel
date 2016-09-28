63816af88d5388f13395bcff0aab9d98d9229365
sjcjoosten <sjcjoosten+github@gmail.com>
2016-09-28 22:40:15 +0200

    Travis update, trying to run tests

--- test/test_all.sh
+++ test/test_all.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
+
 
 rc=0
 for f in *.test
 do
   echo "$f"
-  (sh "$f" | diff -y --suppress-common-lines - "${f%.test}.result") 2>&1 | diff -y -c - "${f%.test}.err"
+  (sh "$f" | diff -y --suppress-common-lines - "${f%.test}.result") 2>&1 | diff -y --suppress-common-lines - "${f%.test}.err"
   rcs=${PIPESTATUS[*]}
   for i in ${rcs}
     do rc=$(($i > $rc ? $i : $rc))
